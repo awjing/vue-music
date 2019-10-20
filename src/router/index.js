8686458da9a1256ffc31e24757d0c32475c36b6e
wangjing90 <wangjing90@jd.com>
2019-10-20 18:44:55 +0800

    add: 项目搭建+jsonp封装

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue'
+import Router from 'vue-router'
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
 
-// Vue.use(Router)
+Vue.use(Router)
 
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'HelloWorld',
-//       component: HelloWorld
-//     }
-//   ]
-// })
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      name: 'Recommend',
+      component: Recommend
+    },
+    {
+      path: '/singer',
+      name: 'Singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      name: 'Rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    }
+  ]
+})
