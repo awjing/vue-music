437ee82dbf8894d05dd7211096a260575b7eb4eb
wangjing90 <wangjing90@jd.com>
2019-10-20 21:19:39 +0800

    add: 轮播图组件开发

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-header">
     <div class="icon"></div>
-    <h1 class="text">Mimi Music</h1>
+    <h1 class="text">Mimi DouDou Music</h1>
   </div>
 </template>
 
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="(item, index) in recommends" :key="index">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl"/>
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -13,15 +20,24 @@
 </template>
 
 <script>
+import Slider from 'base/slider/slider'
 import { getRecommend } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 export default {
+  data () {
+    return {
+      recommends: []
+    }
+  },
+  components: {
+    Slider
+  },
   methods: {
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.slider)
+          this.recommends = res.data.slider
         }
       })
     }
